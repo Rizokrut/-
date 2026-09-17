b0bdec302c22b618589f88f5cfa2242f7d9e114d
Rizokrut <74469592+Rizokrut@users.noreply.github.com>
2026-09-17 23:51:20 +0500

    Add files via upload

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 CHANNEL_ID = os.getenv("CHANNEL_ID", "").strip()  # -100...
-CHANNEL_URL = os.getenv("CHANNEL_URL", "").strip()  # https://t.me/...
+CHANNEL_URL = os.getenv("CHANNEL_URL", "https://t.me/+XbVUN9inkQ4yZDAy").strip()
 
 if not BOT_TOKEN:
     raise RuntimeError("Задай BOT_TOKEN")
